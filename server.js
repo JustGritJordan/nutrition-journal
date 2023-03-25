ef0445d2fa2283c6f7d5aeca58bd9631db3412ec
JGJ <jordan.gb0@gmail.com>
2023-03-25 12:55:14 -0400

    authentication set up complete, schema databases works, able to run

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 // const routes = require('./controllers');
 
-// const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -23,8 +23,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+// app.use(routes); 
 
-// sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-// });
+});
