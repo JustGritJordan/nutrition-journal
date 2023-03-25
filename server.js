d4513037deee486ceccb9a5b44090e79750e6272
JGJ <jordan.gb0@gmail.com>
2023-03-25 15:02:55 -0400

    almost complete final set up with controller

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const config = require('./config/connection');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(session(config.expressSessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(routes); 
+app.use(routes); 
 
 config.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
