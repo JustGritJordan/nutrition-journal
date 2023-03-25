0362dfa48e05512e48f624b98ea4b41e6e240413
JGJ <jordan.gb0@gmail.com>
2023-03-25 15:31:11 -0400

    express server talking to handlebars,working app, pre buildout

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get("/", async(req,res)=>{
+router.get("/", async (req,res) => {
     res.render('home', {name : 'Jordan' });
 }); 
 
-module.export = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const session = require('express-session');
 const config = require('./config/connection');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const app = express();
+const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
 
 app.use(session(config.expressSessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars" , hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes); 
 
 config.sequelize.sync({ force: true }).then(() => {
