f190e3de99158be957223d483893d598d769445d
JGJ <jordan.gb0@gmail.com>
2023-05-04 17:44:39 -0400

    data is being passed to webpage and server

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
-const mealsRoutes = require('./meals')
-const userRoutes = require('./user')
 
-router.use("/user",userRoutes);
-router.use("/meals", mealsRoutes);
+router.post('/login', async (req,res) => {
+   const newUserData = req.body;
+
+   req.session.save(()=>{
+    req.session.loggedIn = true;
+    res.status(201).json({ message: 'New user created!'});
+   });
+
+}); 
+
+
+// const router = require('express').Router();
+// const mealsRoutes = require('./meals')
+// const userRoutes = require('./user')
+
+// router.use("/user",userRoutes);
+// router.use("/meals", mealsRoutes);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -2,19 +2,31 @@ const registerForm = document.querySelector(".register-form");
 
 async function handleRegister(event) {
   event.preventDefault();
-  console.log("you registered!");
 
-  const emailRegister = document
-    .querySelector("#email-register")
-    .value
-    .trim();
-  const passwordRegister = document
-    .querySelector("#password-register")
-    .value
-    .trim();
+  const emailRegister = document.querySelector("#email-register").value.trim();
+  const passwordRegister = document.querySelector("#password-register").value.trim();
 
-    console.log({emailRegister,passwordRegister});
+    const newUser = {
+      email:emailRegister,
+      password: passwordRegister,
+    };
+     
+   
 
+    const response = await fetch("/api/login", {
+    body: JSON.stringify(newUser),
+    method: "POST",
+    headers: { 
+      "content-type": 'application/json',
+    },
+    });
+
+    if (response.ok) {
+      console.log("working!");
+    } else {
+    console.log("not working!");
+    }
+    
     // registerForm.reset();
 }
 registerForm.addEventListener("click", handleRegister);
